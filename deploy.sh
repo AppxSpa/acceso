1cd960f9728b4a596f202204399cab19af3407ae
Mirko Gutierrez <mirko.gutierrez@appx.cl>
2026-01-27 11:16:23 -0300

    actualiza deploy

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,23 @@
 # CONFIGURACIÓN DEL MICROSERVICIO (Actualizado a GHCR)
 # =========================================================
 NOMBRE_APP="acceso"
-PUERTO="8081"
-# Cambiamos la ruta de Docker Hub a GitHub Container Registry
-IMAGEN_HUB="ghcr.io/mirkogutierrez/acceso" 
+PUERTO="8082"
+
+# CORRECCIÓN: Usar la organización AppxSpa en minúsculas
+IMAGEN_HUB="ghcr.io/appxspa/acceso" 
 # =========================================================
 
 OPCION=${1:-"dev"}
 
 case $OPCION in
     "prod")
-        echo "--- MODO PRODUCCIÓN: Bajando de GitHub Packages ($IMAGEN_HUB) ---"
-        # Ahora el pull se hace desde el registro privado de GitHub
+        echo "--- MODO PRODUCCIÓN: Bajando de GHCR ($IMAGEN_HUB) ---"
         docker pull $IMAGEN_HUB:latest
         TARGET_IMAGE="$IMAGEN_HUB:latest"
         ;;
     *)
         echo "--- MODO DESARROLLO: Compilando localmente ($NOMBRE_APP) ---"
+        # Asegúrate de tener permisos en mvnw
         ./mvnw clean package -DskipTests
         docker build -t $NOMBRE_APP:local .
         TARGET_IMAGE="$NOMBRE_APP:local"
@@ -31,15 +32,16 @@ docker stop ${NOMBRE_APP}-container 2>/dev/null
 docker rm ${NOMBRE_APP}-container 2>/dev/null
 
 echo "--- Iniciando contenedor en puerto $PUERTO ---"
+# OJO: Verifica si la red es 'laflorida' o 'appx'
 docker run \
            --restart always \
-           -d -p ${PUERTO}:${PUERTO} \
+           -d \
+           -p ${PUERTO}:${PUERTO} \
            --env-file .env \
-           --network laflorida \
+           --network appx \
            --add-host=host.docker.internal:host-gateway \
            --name ${NOMBRE_APP}-container \
            $TARGET_IMAGE
 
-# Limpia imágenes antiguas para no llenar el disco del PC de la oficina
 docker image prune -f
 echo "--- Proceso Terminado ($OPCION) ---"
